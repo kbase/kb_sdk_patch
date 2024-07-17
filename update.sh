b20baf2f0f0bf7da1cb781e19c699a4e91cb801c
Dakota Blair <dblair@bnl.gov>
2024-07-17 11:10:31 -0400

    Remove unused* data to reduce image size.
    `*` Hopefully. Removed paths include
    - /tmp
    - /boot
    - /usr/lib/firmware
    - /usr/lib/jvm/java-8-openjdk-amd64
      - may need to add this one back in later
    - /usr/share/doc
    - /usr/share/man
    - /usr/share/locale/{locale}
      - for {locale} other than `en`

--- update.sh
+++ update.sh
@@ -32,5 +32,30 @@ for pkg in docker.io docker-doc docker-compose docker-compose-v2 \
 apt install -y curl
 apt autoremove -y
 curl -fsSL https://get.docker.com | sh
+apt remove -y $(dpkg-query --show 'linux-modules-*' | cut -f1 | grep -v "$(uname -r)")
+rm -rf \
+    /tmp \
+    /boot \
+    /usr/lib/firmware \
+    /usr/lib/jvm/java-8-openjdk-amd64 \
+    /usr/share/doc \
+    /usr/share/man
+find /usr/share/locale \
+    -mindepth 1 -maxdepth 1 \
+    -not -name en \
+    -execdir rm -rf {} +
 mkdir /.docker
-chmod 777 /.docker
+mkdir /tmp
+mkdir /boot
+mkdir -p /usr/lib/firmware
+mkdir -p /usr/lib/jvm/java-8-openjdk-amd64
+mkdir -p /usr/share/doc
+mkdir -p /usr/share/man
+chmod 777 \
+    /.docker \
+    /boot \
+    /tmp \
+    /usr/lib/firmware \
+    /usr/lib/jvm/java-8-openjdk-amd64 \
+    /usr/share/doc \
+    /usr/share/man
